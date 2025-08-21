dfd1b0e38141e9882d249fed8e700a191d99bee1
bkop-ds <84399410+bkop-ds@users.noreply.github.com>
2025-08-21 15:46:17 -0400

    Updated Decision in Dacc chair

--- src/pages/dataRequest.js
+++ src/pages/dataRequest.js
@@ -3056,7 +3056,7 @@ export async function viewFinalDecisionFiles(files) {
   let template = "";
 
   for (const fileInfo of files) {
-    console.log(fileInfo);
+    console.log(fileInfo.parent.id);
     const fileId = fileInfo.id;
     let filename = fileInfo.name.slice(0, -4).split("_").join(" ");
     console.log(filename);
@@ -3074,9 +3074,9 @@ export async function viewFinalDecisionFiles(files) {
               .toDateString()
               .substring(4)}</div>
             <div class="col-lg-2 pl-6 text-right">${
-              fileInfo.parent.name === "Accepted"
+              fileInfo.parent.id = acceptedFolder
                 ? '<h6 class="badge badge-pill badge-success">Accepted</h6>'
-                : fileInfo.parent.name === "Denied"
+                : fileInfo.parent.id = deniedFolder
                 ? '<h6 class="badge badge-pill badge-danger">Denied</h6>'
                 : '<h6 class="badge badge-pill badge-warning">Under Review</h6>'
             }</div>
